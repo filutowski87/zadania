084d39168c453785ec8a03833e321ef61a35d568
filutowski87 <122361410+filutowski87@users.noreply.github.com>
2023-02-11 17:09:17 +0100

    commit3

--- długośc stringu/js/main.js
+++ długośc stringu/js/main.js
@@ -11,3 +11,20 @@ function porownaj(arg1, arg2 ) {
     
 }
 console.log(porownaj(nav, nav2));
+
+
+
+const arr = [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 1]
+function sort(arr) {
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < arr.length; j++){
+        if(arr[j] > arr[j + 1]){
+            let bubble = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = bubble;
+        }
+    }
+}
+console.log(arr);
+}
+sort(arr);
